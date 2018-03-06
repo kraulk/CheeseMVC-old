af94244cf781484a6cd53f9a5dcb9bc7087afc7c
kraulk <kristofer.kraul@gmail.com>
2018-03-06 13:06:35 -0500

    Post class 3.8 prep

---
+++ src/CheeseMVC/Models/CheeseData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace CheeseMVC.Models
+{
+    public class CheeseData
+    {
+        static private List<Cheese> cheeses = new List<Cheese>();
+
+        // GetAll
+
+        public static List<Cheese> GetAll()
+        {
+            return cheeses;
+        }
+
+        // Add
+
+        public static void Add(Cheese newCheese)
+        {
+            cheeses.Add(newCheese); 
+        }
+
+        // Remove
+
+        public static void Remove(int id)
+        {
+            Cheese cheeseToRemove = GetById(id);
+            cheeses.Remove(cheeseToRemove);
+        }
+
+
+        // GetById
+
+        public static Cheese GetById(int id)
+        {
+            return cheeses.Single(p => p.CheeseId == id);
+        }
+    }
+}
